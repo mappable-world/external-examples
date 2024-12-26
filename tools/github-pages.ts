c1d3e97dce9d069b21f4ca2f90e698c2815c9623
matthew44-mappable <155086725+matthew44-mappable@users.noreply.github.com>
2024-12-26 16:09:49 +0300

    Add deploy workflow to GitHub Pages (#3)
    * Add deploy workflow to GitHub Pages
    
    * Add check in CI for github pages build
    
    * Add index page
    
    * Add enviroment in deploy job

--- tools/github-pages.ts
+++ tools/github-pages.ts
@@ -1,14 +1,20 @@
 import path from "path";
 import fs from "fs";
-import { getExamples } from "./utils";
+import { Example, getExamples } from "./utils";
 
 const STATIC_FOLDER = path.resolve(__dirname, "../github-pages-static");
+const TEMPLATES_FOLDER = path.resolve(__dirname, "templates");
+const INDEX_PAGE_TEMPLATE_PATH = path.resolve(TEMPLATES_FOLDER, "index.html");
+
+const EXAMPLES_LINKS_LIST_REGEXP = /%EXAMPLES_LINKS_LIST%/;
 
 if (fs.existsSync(STATIC_FOLDER)) {
   fs.rmSync(STATIC_FOLDER, { recursive: true });
 }
 
-getExamples().forEach((example) => {
+const examples = getExamples();
+
+examples.forEach((example) => {
   const exampleDistFolder = path.join(example.path, "dist");
   if (!fs.existsSync(exampleDistFolder)) {
     throw `${example.name} "dist" folder does not exist`;
@@ -18,3 +24,12 @@ getExamples().forEach((example) => {
     recursive: true,
   });
 });
+
+const indexPageContent = fs
+  .readFileSync(INDEX_PAGE_TEMPLATE_PATH, "utf8")
+  .replace(EXAMPLES_LINKS_LIST_REGEXP, getLinkList(examples).join(""));
+fs.writeFileSync(path.join(STATIC_FOLDER, "index.html"), indexPageContent);
+
+export function getLinkList(examples: Example[]): string[] {
+  return examples.map(({ name }) => `<a href="${name}/index.html">${name}</a>`);
+}
