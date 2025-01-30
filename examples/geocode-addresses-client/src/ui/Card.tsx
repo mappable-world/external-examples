4cc37886630f1e87364f1d55f0c76d9551429385
matthew44-mappable <155086725+matthew44-mappable@users.noreply.github.com>
2025-01-30 21:14:43 +0300

    Fix design in geocode-addresses-client (#5)
    * Fix design in geocode-addresses-client
    
    * fixed
    
    * bacdor close by click
    
    * fix 2
    
    * fix 5
    
    * update files

--- examples/geocode-addresses-client/src/ui/Card.tsx
+++ examples/geocode-addresses-client/src/ui/Card.tsx
@@ -1,23 +1,21 @@
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 
-import styles from '../styles/ui/Card.module.css';
+import styles from "../styles/ui/Card.module.css";
 
 const cx = classNames.bind(styles);
 
 type Props = {
-    title: string;
-    count: number;
-    color: string;
+  title: string;
+  count: number;
+  color: string;
 };
 
-export function Card({title, count, color}: Props) {
-    return (
-        <div className={styles.card}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.content}>
-                <div className={styles.count}>{count}</div>
-                <div className={cx('circle', color)} />
-            </div>
-        </div>
-    );
+export function Card({ title, count, color }: Props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.count}>{count}</div>
+      <div className={cx("circle", color, "circleCard")} />
+    </div>
+  );
 }
--- examples/geocode-addresses-client/src/ui/Hint.tsx
+++ examples/geocode-addresses-client/src/ui/Hint.tsx
@@ -1,70 +1,87 @@
-import {useState} from 'react';
-import {Sheet} from 'react-modal-sheet';
+import { useState } from "react";
+import { Sheet } from "react-modal-sheet";
 
-import InfoIcon from '../icons/info.svg';
+import InfoIcon from "../icons/info.svg";
 
-import styles from '../styles/ui/Hint.module.css';
+import styles from "../styles/ui/Hint.module.css";
 
 type Props = {
-    popupContent?: boolean;
+  popupContent?: boolean;
 };
 
-export function Hint({popupContent = false}: Props) {
-    const [active, setActive] = useState(false);
-    const [open, setOpen] = useState(false);
+export function Hint({ popupContent = false }: Props) {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const onClick = () => {
-        if (popupContent) {
-            setOpen(!open);
-        } else {
-            setActive(true);
-        }
-    };
+  const onClick = () => {
+    if (popupContent) {
+      setOpen(!open);
+    } else {
+      setActive(true);
+    }
+  };
 
-    const onMouseEnter = () => {
-        if (!popupContent) {
-            setActive(true);
-        }
-    };
+  const onMouseEnter = () => {
+    if (!popupContent) {
+      setActive(true);
+    }
+  };
 
-    const onMouseLeave = () => {
-        if (!popupContent) {
-            setActive(false);
-        }
-    };
+  const onClosePopup = () => {
+    if (!popupContent) {
+      setActive(false);
+    }
+  };
 
-    const content = (
-        <>
-            First of all, the Geocoder API converts addresses into coordinates, which you can see in the XLSX file
-            available for download. Then, that file is converted into JSON.
-            <br />
-            <br />
-            The API may offer you the closest building if possible, as it takes into account common misspellings. In
-            some cases, it can only identify a street or city; this might be due to a mistake, or because a building is
-            new or has been demolished.
-        </>
-    );
+  const content = (
+    <>
+      First of all, the Geocoder API converts addresses into coordinates, which
+      you can see in the XLSX file available for download. Then, that file is
+      converted into JSON.
+      <br />
+      <br />
+      The API may offer you the closest building if possible, as it takes into
+      account common misspellings. In some cases, it can only identify a street
+      or city; this might be due to a mistake, or because a building is new or
+      has been demolished.
+    </>
+  );
 
-    return (
-        <div className={styles.hint} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
-            <InfoIcon />
-            <span className={styles.button}>How it works</span>
+  return (
+    <div
+      className={styles.hint}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onClosePopup}
+      onClick={onClick}
+    >
+      <InfoIcon />
+      <span className={styles.button}>How it works</span>
 
-            <Sheet isOpen={open} detent="content-height" onClose={() => setOpen(false)}>
-                <Sheet.Container style={{borderTopRightRadius: 24, borderTopLeftRadius: 24}}>
-                    <Sheet.Header>
-                        <div className="modal-container">
-                            <div className="modal-control" />
-                        </div>
-                    </Sheet.Header>
-                    <Sheet.Content>
-                        <div className={styles.info}>{content}</div>
-                    </Sheet.Content>
-                </Sheet.Container>
-                <Sheet.Backdrop />
-            </Sheet>
+      <Sheet
+        isOpen={open}
+        detent="content-height"
+        onClose={() => setOpen(false)}
+      >
+        <Sheet.Backdrop
+          style={{ backgroundColor: "rgba(5, 13, 51, 0.40)" }}
+          onTap={() => setOpen(false)}
+        />
+        <Sheet.Container
+          style={{ borderTopRightRadius: 24, borderTopLeftRadius: 24 }}
+        >
+          <Sheet.Header>
+            <div className="modal-container">
+              <div className="modal-control" />
+            </div>
+          </Sheet.Header>
+          <Sheet.Content>
+            <div className={styles.info}>{content}</div>
+          </Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
 
-            {active && <div className={styles.content}>{content}</div>}
-        </div>
-    );
+      {active && <div className={styles.content}>{content}</div>}
+    </div>
+  );
 }
